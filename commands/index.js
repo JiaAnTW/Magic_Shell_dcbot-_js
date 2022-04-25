a4b49fb68d1624ef2e0134b3e0604690cdac1e00
JiaAnTW <at5lp6andy@gmail.com>
2022-04-25 23:37:32 +0800

    ci: update npm start

--- commands/index.js
+++ commands/index.js
@@ -2,9 +2,9 @@ const { join, leave, setTriggerPosibility, random } = require('./io.js');
 const { help } = require('./help.js');
 
 module.exports.commands = {
-    '?阿致回來': join,
+    /*'?阿致回來': join,
     '?阿致嘴閉閉': leave,
-    '?阿致活躍度': setTriggerPosibility,
-    '?阿致能幹嘛': help,
+    '?阿致活躍度': setTriggerPosibility,*/
+    '!chi能幹嘛': help,
     '!chi抽籤': random,
 };
