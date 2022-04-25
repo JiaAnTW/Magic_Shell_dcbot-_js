f3b64bed4e0892ceb228c6f3d08d3d4f0479cb12
JiaAnTW <at5lp6andy@gmail.com>
2022-04-25 22:54:15 +0800

    feat: become chi

--- commands/index.js
+++ commands/index.js
@@ -1,4 +1,4 @@
-const { join, leave, setTriggerPosibility } = require('./io.js');
+const { join, leave, setTriggerPosibility, random } = require('./io.js');
 const { help } = require('./help.js');
 
 module.exports.commands = {
@@ -6,4 +6,5 @@ module.exports.commands = {
     '?阿致嘴閉閉': leave,
     '?阿致活躍度': setTriggerPosibility,
     '?阿致能幹嘛': help,
+    '!chi抽籤': random,
 };
--- commands/io.js
+++ commands/io.js
@@ -14,6 +14,12 @@ function setLock(value) {
     lock = value;
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is
+}
+
 module.exports.setTriggerPosibility = async function setTriggerPosibility(
     message,
     client,
@@ -32,6 +38,36 @@ module.exports.setTriggerPosibility = async function setTriggerPosibility(
         triggerPosibility = value;
     } catch (err) {
         console.log(err);
+    }
+};
+
+module.exports.random = async function join(
+    message,
+    client,
+    connection,
+    setConnection
+) {
+    try {
+        const cilentCommand = message.content.split(' ');
+        console.log(cilentCommand);
+        let ctn = Number(cilentCommand[1]);
+        let pool = cilentCommand.slice(2);
+        let res = [];
+        while (ctn > 0 && 0 < pool.length) {
+            let i = getRandomInt(0, pool.length - 1);
+            if (!pool[i] || pool[i] === '') {
+                pool.splice(i, 1);
+                continue;
+            }
+            res.push(pool[i]);
+            pool.splice(i, 1);
+            ctn--;
+        }
+        console.log(res);
+        message.reply(`抽籤結果: ${res.join(',')}`);
+    } catch (error) {
+        console.warn(error);
+        setConnection(undefined);
     }
 };
 
